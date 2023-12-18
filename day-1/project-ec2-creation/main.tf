35e336cf8bdae23624ccb1ef42eab00516ea29c5
PrachiJukaria <74362259+PrachiJukaria@users.noreply.github.com>
2023-12-18 13:36:37 +0000

    main.tf file added

---
+++ day-1/project-ec2-creation/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+ ami           = "ami-0fc5d935ebf8bc3bc"  # Specify an appropriate AMI ID
+ instance_type = "t2.micro"
+ 
+}
